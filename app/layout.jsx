88df86dfd9914a82b190a11a076cede6f9fb2445
mhdwahyusiregar <siregarwahyu573@gmail.com>
2024-05-14 08:55:02 +0700

    initial commit

---
+++ app/layout.jsx
@@ -0,0 +1,10 @@
+export default function Layout({ children }) {
+  return (
+    <html lang="en">
+      <head>
+        <title>Learn NextJS Fundamental</title>
+      </head>
+      <body>{children}</body>
+    </html>
+  );
+}
