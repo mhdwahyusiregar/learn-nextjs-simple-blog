ac2f0dd8b32aeef9ac7861774f568909728be265
mhdwahyusiregar <siregarwahyu573@gmail.com>
2024-05-18 09:30:26 +0700

    feat: implementasi strapi dalam mode dev

---
+++ scripts/strapi-request.mjs
@@ -0,0 +1,24 @@
+import { writeFileSync } from 'node:fs';
+import qs from 'qs';
+
+const url =
+  'http://localhost:1337/api/posts' +
+  '?' +
+  qs.stringify(
+    {
+      fields: ['slug', 'title', 'description', 'publishedAt', 'body', 'author'],
+      populate: {
+        image: { fields: ['url'] },
+      },
+      sort: ['publishedAt:desc'],
+      pagination: { pageSize: 3 },
+    },
+    { encodeValuesOnly: true },
+  );
+const response = await fetch(url);
+const body = await response.json();
+const posts = JSON.stringify(body, null, 2);
+// console.log(posts);
+
+const file = 'scripts/strapi-response.json';
+writeFileSync(file, posts, 'utf8');
